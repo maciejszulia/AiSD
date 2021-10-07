0656358d0813bc4bf302ae24e72e079c795d4f53
maciejszulia <maciej.szulia@gmail.com>
2021-10-08 01:25:16 +0200

    into lab1

--- 1.py
+++
@@ -1,9 +0,0 @@
-# Przygotować funkcję, która przyjmie w parametrach pierwszą literę imienia,
-# oraz nazwisko, a następnie zwróci te wartości połączone kropką.
-# Przykładowe wyjście: J. Kowalski.
-
-def foo(imie: str, nazwisko: str):
-    return imie[0] + ". " + nazwisko
-
-
-print(foo("jan", "kowalski"))
--- 8.py
+++
@@ -1,9 +0,0 @@
-# Przygotować skrypt, w którym użytkownik będzie wprowadzał do listy wartości używając pętli,
-# a następnie wartości z tej zostanią zrzutowane do krotki.
-
-lista = []
-while (len(lista) != 10):
-    x = input()
-    lista.append(x)
-
-print(tuple(lista))

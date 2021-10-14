ae931740d2d12b691685513a312f08d14105d74d
maciejszulia <maciej.szulia@gmail.com>
2021-10-14 20:58:55 +0200

    camelcase to undescorecase

--- lab1/9.py
+++ lab1/9.py
@@ -3,8 +3,8 @@
 # Nie należy w tym zadaniu używać instrukcji warunkowych! Przykładowo, wejście: 6, wyjście: sobota.
 
 def foo(i: int):
-    dniTygodnia = ["poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela"]
-    return dniTygodnia[i - 1]
+    dni_tygodnia = ["poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela"]
+    return dni_tygodnia[i - 1]
 
 
 print(foo(6))

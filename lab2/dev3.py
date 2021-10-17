93cc12174d8368d32f2d91b443eb533964d2731b
maciejszulia <maciej.szulia@gmail.com>
2021-10-17 14:51:59 +0200

    resolved if this_node.next is not None

--- lab2/dev3.py
+++ lab2/dev3.py
@@ -16,10 +16,18 @@ class LinkedList:
         output = ""
         this_node = self.head
         while this_node is not None:
-            output += str(Node.value)
+            output += str(this_node.value)
             if this_node.next is not None:
                 output += " -> "
+            this_node = this_node.next
         return output
+
+    def push(self, value: Any) -> None:
+        this_node = Node(value)
+        this_node.next = self.head
+        self.head = this_node
+        if self.tail is None:
+            self.tail = this_node
 
     def print(self):
         print(str(self.__repr__()))
@@ -27,4 +35,9 @@ class LinkedList:
 
 list_ = LinkedList()
 assert list_.head == None
+
+list_.push(1)
+list_.push(0)
+
+#assert str(list_) == '0 -> 1'
 LinkedList.print(self=list_)

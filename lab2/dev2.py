80a0e4d2d9482ce8c9507097b768090493ca49ca
maciejszulia <maciej.szulia@gmail.com>
2021-10-18 15:22:22 +0200

    nowy sposob

--- lab2/dev2.py
+++ lab2/dev2.py
@@ -1,9 +1,48 @@
-class node:
-    def __init__(self, value):
-        self.value = value
-        self.next_node = None
+from typing import Any
 
-class linked_list:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+
+class Node:
+    def __init__(self, value: Any, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+    def __repr__(self):
+        output = ""
+        current_node = self.head
+        while current_node is not None:
+            output += str(current_node.value)
+            if current_node.next_node is not None:
+                output += " -> "
+            current_node = current_node.next_node
+        return output
+
+    def print(self):
+        print(str(self.__repr__()))
+
+    def append(self, value: Any) -> None:
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while True:
+            if current_node.next_node is None:
+                current_node.next_node = new_node
+                break
+            current_node = current_node.next_node
+
+    def push(self, value: Any) -> None:
+        new_node = Node(value)
+
+
+list_ = LinkedList()
+assert list_.head is None
+list_.append(9)
+list_.append(10)
+assert str(list_) == '9 -> 10'

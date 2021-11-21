89406754a6e7e4dbf4dec8809da3df228c71709a
maciejszulia <maciej.szulia@gmail.com>
2021-11-21 20:10:48 +0100

    added zad 05 a chuj tam

---
+++ lab03-rekurencja/zad05.py
@@ -0,0 +1,15 @@
+# Zaimplementować funkcję factorial(n: int) -> int,
+# która zwróci silnię wartości przekazanej w parametrze
+
+def factorial(n: int) -> int:
+    if n == 0 or n == 1:
+        return 1
+    if n < 0:
+        print("ERROR")
+        return None
+    return n * factorial(n - 1)
+
+
+print(factorial(-10))
+print(factorial(10))
+print(factorial(0))

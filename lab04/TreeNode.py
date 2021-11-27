f291d05deac634b63bb6ee8243aea373d774183d
Maciej Szulia <maciej.szulia@gmail.com>
2021-11-27 18:00:50 +0100

    TreeNode todo

--- lab04/TreeNode.py
+++ lab04/TreeNode.py
@@ -9,15 +9,16 @@ class TreeNode:
     def __init__(self, value):
         self.value = value
         self.children = []
+        self.parent = None
 
     def __str__(self):
         return str(self.value)
 
     def is_leaf(self) -> bool:
-        if len(self.children) == 0:
+        if self.children:
             return True
-
-        return False
+        else:
+            return False
 
     def add(self, child: 'TreeNode') -> None:
         self.children.append(child)
@@ -30,24 +31,40 @@ class TreeNode:
 
     def for_each_level_order(self, visit: Callable[['TreeNode'], None]) -> None:
         visit(self)
-        _queue = Queue()
 
+        _queue = Queue()
         for child in self.children:
             _queue.enqueue(child)
 
-        while len(_queue) != 0:
-            top_elem = _queue.dequeue()
-            visit(top_elem)
-
-            for i in top_elem.childern:
+        while _queue:  # while len(_queue) != 0:
+            this_node = _queue.dequeue()
+            visit(this_node)
+            for i in this_node.childern:
                 _queue.enqueue(i)
 
+    #todo da sie to lepiej
     def search(self, value: Any) -> Union['TreeNode', None]:
+        output: List[TreeNode] = []
 
-        def fun():
-            output: List['TreeNode'] = [TreeNode.value]
-            return output
+        def fun(node: 'TreeNode'):
+            if node.value == value:
+                output.append(node)
+
+        self.for_each_level_order(fun)
+
+        if output:
+            return output[0]
+        else:
+            return None
 
 
-class Tree:
-    root: TreeNode
+tree: 'TreeNode' = TreeNode(2)
+assert tree.value == 2
+tree.add(TreeNode(1))
+assert tree.children[0].value == 1
+tree.add(TreeNode(4))
+assert tree.children[1].value == 4
+tree.children[0].add(TreeNode(3))
+assert tree.children[0].children[0].value == 3
+tree.children[1].add(TreeNode(6))
+assert tree.children[1].children[0].value == 6
